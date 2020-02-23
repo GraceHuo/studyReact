851934edbb467670c67e9342a2e5c8dc7a411d6a
Yan Huo <UA33443@DOW.COM>
2020-02-22 18:25:04 -0600

    test antd

--- study-react-1/src/App.js
+++ study-react-1/src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
-
+// import Button from "antd/es/button";
+// import "antd/dist/antd.css";
+import { Button } from "antd";
 function App() {
-  return <div className="App">app</div>;
+  return (
+    <div className="App">
+      <Button type="primary">Submit</Button>
+    </div>
+  );
 }
 
 export default App;
